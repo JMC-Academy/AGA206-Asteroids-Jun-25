c9666f540443808129ee48a1cd711260a13d455a
JMC-Academy <bvotano@myjmc.edu.au>
2025-06-26 10:15:20 +1000

    Spaceship Damage and Asteroid Script

--- Assets/Scripts/Spaceship.cs
+++ Assets/Scripts/Spaceship.cs
@@ -4,12 +4,15 @@ public class Spaceship : MonoBehaviour
 {
     public float EnginePower = 10f;
     public float TurnPower = 10f;
+    public int HealthMax = 3;
+    public int HealthCurrent;
 
     private Rigidbody2D rb2D;
 
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        HealthCurrent = HealthMax;
     }
 
     void Update()
@@ -19,6 +22,11 @@ public class Spaceship : MonoBehaviour
 
         ApplyThrust(vertical);
         ApplyTorque(horizontal);
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            TakeDamage(1);
+        }
     }
 
     private void ApplyThrust(float amount)
@@ -33,5 +41,25 @@ public class Spaceship : MonoBehaviour
         //Debug.Log("Torque amount is " + amount);
         float torque = amount * TurnPower * Time.deltaTime;
         rb2D.AddTorque(-torque);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        //Reduce the current health by the damage
+        HealthCurrent = HealthCurrent - damage;
+        //HealthCurrent -= damage; another way of writing the above
+
+        //If current health is zero, then Explode
+        if(HealthCurrent <= 0)
+        {
+            Explode();
+        }
+    }
+
+    public void Explode()
+    {
+        //Destroy the ship, end the game
+        Debug.Log("Game Over");
+        Destroy(gameObject);
     }
 }
